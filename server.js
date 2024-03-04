9c49d914c0fb226e55a9a69cf63ad1ecd035d366
Jaimin Patel <jaiminpatel.2309@gmail.com>
2024-03-04 17:40:53 +0530

    roomtype change in server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.post('/api/lifestyle', async (req, res) => {
   try {
     const query = {};
     if (roomType) {
-      query.roomType =  roomType ;
+      query.roomType = roomType ;
     }
     if (color) {
       query.color = { $in: color };
